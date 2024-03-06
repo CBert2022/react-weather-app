0f7acb8f771f606c5d90cd681dced082b5a599a9
“CaroBert“ <carolin.rieche@yahoo.de>
2024-03-06 22:25:20 +0100

    logik für Switch day/night mode, wenn stadt in anderer zeitzone gesucht wird

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,14 +10,15 @@ import Wind from "./assets/wind.png";
 import Stars from "./assets/moon.png";
 import Cloudy from "./assets/cloudy.png";
 import Feel from "./assets/thermometer.png";
-// import More from "./assets/ellipsis.png";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-// import Details from "./Details";
 
+// Definition der Home-Komponente
 function Home() {
+  // Zugriff auf die API-Schlüssel aus den Umgebungsvariablen
   const { REACT_APP_API_KEY } = process.env;
 
+  // Zustände für die Wetterdaten, die Eingabestadt und Fehlermeldungen
   const [data, setData] = useState({
     celsius: 10,
     name: "Suche Stadt...",
@@ -27,19 +28,36 @@ function Home() {
     feel: 10,
     sunrise: 0,
     sunset: 0,
-    descripiton: "",
+    description: "",
   });
-
   const [name, setName] = useState("");
   const [error, setError] = useState("");
+  // Zustand, um zu überprüfen, ob es Nacht ist
   const [isNight, setIsNight] = useState(false);
+  // Zustand, um zu überprüfen, ob bereits eine Stadt abgerufen wurde
+  const [cityFetched, setCityFetched] = useState(false);
 
-  // falls geolocation vom user erlaubt:
+  // Effekt, um die Geolokalisierung auszuführen, wenn keine Stadt abgerufen wurde
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(zeigePosition);
-  }, [isNight]); //  ins array als abhängigkeit => Effekt wird erneut gerenderd wenn isNight sich ändert
+    if (!cityFetched && name === "") {
+      navigator.geolocation.getCurrentPosition(zeigePosition);
+    }
+  }, [name, cityFetched]);
 
-  // koordinaten speichern zur Abfrage der stadt
+  // // Effekt, um die Geolokalisierung basierend auf isNight zu aktualisieren
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition(zeigePosition);
+  //   fetchData(data.name); // Hier rufen wir die API für die Stadt an, die bereits abgerufen wurde
+  // }, [isNight]);
+
+  // Effekt, um die Geolokalisierung basierend auf isNight zu aktualisieren
+  useEffect(() => {
+    if (cityFetched) {
+      fetchData(data.name); // Hier rufen wir die API für die Stadt an, die bereits abgerufen wurde
+    }
+  }, [isNight]);
+
+  // Funktion zur Verarbeitung der Geolokalisierungsdaten
   function zeigePosition(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
@@ -50,8 +68,8 @@ function Home() {
       .get(apiURL)
       .then((res) => {
         const city = res.data[0].name;
-        // übergabe der ermittelten stadt aus geolocation an fetchData
         fetchData(city);
+        setCityFetched(true);
       })
       .catch((err) => {
         if (err.response.status === 404) {
@@ -59,11 +77,10 @@ function Home() {
         } else {
           setError("");
         }
-        // console.log(err);
       });
   }
 
-  // definition der fetchData fkt
+  // Funktion zum Abrufen von Wetterdaten basierend auf dem Stadtnamen
   function fetchData(cityName) {
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${REACT_APP_API_KEY}&units=metric&lang=de`;
 
@@ -71,26 +88,17 @@ function Home() {
       .get(apiURL)
       .then((res) => {
         let imagePath = "";
-        console.log(res.data);
 
-        // Ermittlung ob Tag oder Nacht anhand der gelieferten Zeiten aus der API
-        function dayOrNight() {
-          let sunset = res.data.sys.sunset;
-          let currentTime = Math.floor(Date.now() / 1000);
-          if (currentTime >= sunset) {
-            console.log("Die Sonne ist bereits untergegangen.");
-            setIsNight(true); // Setze den Nachtstatus auf true
-          } else {
-            console.log("Die Sonne ist noch nicht untergegangen.");
-            setIsNight(false); // Setze den Nachtstatus auf false
-          }
-        }
+        // Bestimmung von Tag oder Nacht
 
-        dayOrNight();
+        const sunriseTime = res.data.sys.sunrise;
+        const sunsetTime = res.data.sys.sunset;
+        let currentTime = Math.floor(Date.now() / 1000);
+        const night = currentTime >= sunsetTime || currentTime < sunriseTime;
+        setIsNight(night);
 
-        if (isNight) {
+        if (night) {
           document.querySelector(".weather").classList.add("night");
-          //hintergrundfarbe in dunkel ändern
           if (res.data.weather[0].main === "Clouds") {
             imagePath = Cloud;
           } else if (res.data.weather[0].main === "Clear") {
@@ -109,7 +117,6 @@ function Home() {
             imagePath = Cloudy;
           }
         } else {
-          // Tagsüber, behalte den Hintergrund und Bilder bei oder ändere sie entsprechend
           document.querySelector(".weather").classList.remove("night");
           if (res.data.weather[0].main === "Clouds") {
             imagePath = Cloud;
@@ -131,7 +138,7 @@ function Home() {
         }
 
         setData({
-          ...data, // kopiere vorhandene daten und aktualisiere
+          ...data,
           celsius: res.data.main.temp,
           name: res.data.name,
           humidity: res.data.main.humidity,
@@ -140,7 +147,7 @@ function Home() {
           feel: res.data.main.feels_like,
           sunrise: res.data.sys.sunrise,
           sunset: res.data.sys.sunset,
-          descripiton: res.data.weather[0].description,
+          description: res.data.weather[0].description,
         });
         setError("");
       })
@@ -150,16 +157,18 @@ function Home() {
         } else {
           setError("");
         }
-        console.log(err);
       });
   }
 
+  // Funktion zum Verarbeiten von Klickereignissen
   const handleClick = () => {
+    setCityFetched(false); // Setzen des Zustands, um sicherzustellen, dass die Geolokalisierung erneut durchgeführt wird
     if (name !== "") {
       fetchData(name);
     }
   };
 
+  // Rückgabe der JSX-Struktur der Home-Komponente
   return (
     <div className="container">
       <div className="current_lo"></div>
@@ -169,7 +178,6 @@ function Home() {
             id="input"
             type="text"
             placeholder="Enter City Name"
-            // update die stadt im name / setname usestate
             onChange={(e) => setName(e.target.value)}
           />
           <button onClick={handleClick}>
@@ -183,7 +191,7 @@ function Home() {
           <img src={data.image} alt="cloud" />
           <h1>{Math.round(data.celsius)}°C</h1>
           <h2>{data.name}</h2>
-          <p className="description">{data.descripiton}</p>
+          <p className="description">{data.description}</p>
           <div className="details">
             <div className="col">
               <div>
@@ -214,16 +222,7 @@ function Home() {
                 <p>Luftfeuchtigkeit</p>
               </div>
             </div>
-            {/* <div className="col">
-              <div>
-                <img src={More} alt="wind" />
-              </div>
-              <div>
-                <p>More </p>
-              </div>
-            </div> */}
           </div>
-          {/* <Details /> */}
         </div>
         <div className="copyright-mobil">
           <a
@@ -246,4 +245,5 @@ function Home() {
   );
 }
 
+// Export der Home-Komponente als Standard
 export default Home;
